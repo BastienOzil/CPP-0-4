bb4036bf9e12adb639088a3b17c1a41e6e92d3b2
BastienOzil <bastien.ozil17@gmail.com>
2026-02-04 01:13:23 +0100

    txt

--- CPP2/ex03/main.cpp
+++ CPP2/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: bozil <bozil@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/30 14:40:10 by bozil             #+#    #+#             */
-/*   Updated: 2026/01/31 11:17:03 by bozil            ###   ########.fr       */
+/*   Updated: 2026/02/03 11:13:32 by bozil            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,6 @@ void testPoint(Point const &a, Point const &b, Point const &c, Point const &poin
 
 int main(void)
 {
-    // Triangle with vertices at (0,0), (10,0), (5,10)
     Point a(0.0f, 0.0f);
     Point b(10.0f, 0.0f);
     Point c(5.0f, 10.0f);
@@ -38,31 +37,24 @@ int main(void)
     std::cout << "=== BSP (Binary Space Partitioning) Tests ===" << std::endl
               << std::endl;
 
-    // Test 1: Point clearly inside
     Point p1(5.0f, 5.0f);
     testPoint(a, b, c, p1, "Point inside triangle");
 
-    // Test 2: Point outside
     Point p2(15.0f, 5.0f);
     testPoint(a, b, c, p2, "Point outside triangle");
 
-    // Test 3: Point on vertex
     Point p3(0.0f, 0.0f);
     testPoint(a, b, c, p3, "Point on vertex A");
 
-    // Test 4: Point on edge
     Point p4(5.0f, 0.0f);
     testPoint(a, b, c, p4, "Point on edge AB");
 
-    // Test 5: Another point inside
     Point p5(6.0f, 3.0f);
     testPoint(a, b, c, p5, "Another point inside");
 
-    // Test 6: Another point outside
     Point p6(0.0f, 15.0f);
     testPoint(a, b, c, p6, "Another point outside");
 
-    // Test 7: Point very close to edge but inside
     Point p7(5.1f, 5.0f);
     testPoint(a, b, c, p7, "Point close to edge (should be inside)");
 
